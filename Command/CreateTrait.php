a4e34529fcef379793ad340c1bd33e9479025775
Oliver Davies <oliver@oliverdavies.uk>
2016-09-10 08:20:12 +0100

    Show part of the path for confirmation

--- Command/CreateTrait.php
+++ Command/CreateTrait.php
@@ -11,14 +11,18 @@ trait CreateTrait
     private function createFile(
       InputInterface $input,
       OutputInterface $output,
-      $subDir = '',
-      $content = ''
+      $subDir,
+      $content
     ) {
         $path[] = $this->getContainer()->getParameter('sculpin.source_dir');
         $path[] = $subDir;
         $path[] = $filename = $input->getOption('filename');
 
+        // The absolute path to the new file.
         $path = implode(DIRECTORY_SEPARATOR, $path);
+
+        // A shorter path to the new file, relative to the site root.
+        $shortPath = str_replace(getcwd() . '/', '', $path);
 
         /** @var Filesystem $filesystem */
         $filesystem = $this->getContainer()->get('filesystem');
@@ -26,9 +30,9 @@ trait CreateTrait
         if (!$filesystem->exists($path) || $input->getOption('force')) {
             $filesystem->dumpFile($path, $content);
 
-            $output->writeln('<info>' . sprintf('%s has been created.', $filename) . '</info>');
+            $output->writeln('<info>' . sprintf('%s has been created.', $shortPath) . '</info>');
         } else {
-            $output->writeln('<error>' . sprintf('%s already exists.', $filename) . '</error>');
+            $output->writeln('<error>' . sprintf('%s already exists.', $shortPath) . '</error>');
         }
     }
 }
